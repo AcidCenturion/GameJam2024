3f0b09f0eb71b79dbd14449200e1adb77a1d97c8
MiltonTomasino <jonathantomasino13@gmail.com>
2024-03-02 13:56:57 -0800

    player movement + player camera + temp assets
    When adding assets, make sure the ground/platforms are tagged as "Platform" so the jumping works. The temp player object has 2 colliders, one for moving, and another to check for ground/platform.  The check function is accessed by checking "Is Trigger" in the collider settings.

--- Assets/Player Controls/Basic Movement.cs
+++ Assets/Player Controls/Basic Movement.cs
@@ -5,22 +5,61 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     Rigidbody2D rb;
-    float moveForce = 2;
-    private float jumpForce = 9;
+
+    // SerializedField means these values can be changed from Unity interface under this script
+    [SerializeField] float moveForce;
+    [SerializeField] private float jumpForce;
+
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Input.GetAxis("Horizontal") * moveForce * transform.right, ForceMode2D.Force);
+        /*rb.AddForce(Input.GetAxis("Horizontal") * moveForce * transform.right, ForceMode2D.Force);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }*/
+
+        // a & d or left arrow key & right arrow key for left and right
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * moveForce, 0f), ForceMode2D.Impulse);
+        }
+
+        // W & Space bar are jump buttons
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && !isJumping)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+        }
+
+
+    }
+
+    // detects when collider makes contact with object with tag "Platform"
+    // set isJumping to true since this means they are on the ground and can jump
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
+        }
+    }
+
+    // detects when colliderno longer makes contact with object with tag "Platform"
+    // set isJumping to false since it means they jumped and can't double jump
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = true;
         }
     }
 }
